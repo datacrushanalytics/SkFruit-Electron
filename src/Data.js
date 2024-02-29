11466b798662824aa183fe1ba5d5eff9de5942bf
ahernikita <nikkiaher0557@gmail.com>
2024-02-29 17:13:17 +0530

    second commit

---
+++ src/Data.js
@@ -0,0 +1,17 @@
+function login5() {
+    console.log("login function executed");
+
+fetch('http://3.109.121.46/paymentData')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
