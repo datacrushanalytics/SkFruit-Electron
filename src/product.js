eaa624dce3fe99c01e6b1928724ceb09d17e9e6d
ahernikita <nikkiaher0557@gmail.com>
2024-03-01 15:12:54 +0530

    third commit

---
+++ src/product.js
@@ -0,0 +1,37 @@
+function product() {
+    console.log("products function executed");
+
+    fetch('http://3.109.121.46/productData')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            populateTable(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function populateTable(data) {
+    var tbody = document.getElementById('tableBody');
+    tbody.innerHTML = ''; // Clear existing rows
+    var columnsToDisplay = ['name', 'rate'];
+    var counter = 1;
+    data.forEach(function(item) {
+        var row = tbody.insertRow();
+        var cell = row.insertCell();
+        cell.textContent = counter++;
+        columnsToDisplay.forEach(function(key) {
+            var cell = row.insertCell();
+            cell.textContent = item[key];
+        });
+    });
+}
+
+
+product();
