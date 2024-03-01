47f309e52fcca3c37bb4a922c744677cca1b05b6
Dipali Ramesh Wagh <waghdipali398@gmail.com>
2024-03-01 13:33:31 +0530

    third Commit

---
+++ src/data.js
@@ -0,0 +1,20 @@
+function login() {
+    console.log("login function executed");
+
+fetch('http://15.207.221.12/userData')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+
+
