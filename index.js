3fdc2c92cf6994c56f4a436b7ad5cabaf3d16691
SanjyotBhangare <sanjyotbhangare@gofynd.com>
2024-03-24 01:44:51 +0530

    ttt

--- index.js
+++ index.js
@@ -47,8 +47,6 @@
 //   }
 // });
 
-// // In this file you can include the rest of your app's specific main process
-// // code. You can also put them in separate files and import them here.
 
 
 const { app, BrowserWindow } = require('electron');
